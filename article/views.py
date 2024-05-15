f8566e149bbc5cd2037bacaa6e581c665cf15a25
Alex <alexito837@gmail.com>
2024-05-15 16:31:56 -0400

    Modified templates post from database.

--- article/views.py
+++ article/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Article
 
 # Create your views here.
 
 
 def post_list(request):
-    return render(request, "article/post_list.html", {})
+    articles = Article.objects.filter(published_date__lte=timezone.now()).order_by(
+        "published_date"
+    )
+    return render(request, "article/post_list.html", {"articles": articles})
