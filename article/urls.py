21b438a88e053f812ad159a50513360ab3cefa21
Alex <alexito837@gmail.com>
2024-05-16 14:08:47 -0400

    Added views to create/edt blog post inside the site.

--- article/urls.py
+++ article/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.post_list, name="post_list"),
     path("article/<int:pk>/", views.post_detail, name="post_detail"),
+    path("article/new/", views.post_new, name="post_new"),
+    path("article/<int:pk>/edit/", views.post_edit, name="post_edit"),
 ]
--- article/views.py
+++ article/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Article
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from .forms import PostForm
 
 # Create your views here.
 
@@ -16,3 +16,33 @@ def post_list(request):
 def post_detail(request, pk):
     article = get_object_or_404(Article, pk=pk)
     return render(request, "article/post_detail.html", {"article": article})
+
+
+def post_new(request):
+
+    if request.method == "POST":
+        form = PostForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect("post_detail", pk=article.pk)
+    else:
+        form = PostForm()
+    return render(request, "article/post_edit.html", {"form": form})
+
+
+def post_edit(request, pk):
+    article = get_object_or_404(Article, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.author = request.user
+            article.published_date = timezone.now()
+            article.save()
+            return redirect("post_detail", pk=article.pk)
+    else:
+        form = PostForm(instance=article)
+    return render(request, "article/post_edit.html", {"form": form})
